9ed975a5482ae203f39ce5e11242f2f21a7a6514
Katerina <nezkate@yandex.ru>
2026-05-01 20:03:44 +0300

    lab3

--- user-service/src/main/java/com/misis/archapp/user/service/UserService.java
+++ user-service/src/main/java/com/misis/archapp/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.misis.archapp.user.service;
 
+import com.misis.archapp.contract.dto.UserCreatedEvent;
 import com.misis.archapp.user.db.User;
 import com.misis.archapp.user.db.UserRepository;
 import com.misis.archapp.user.dto.UserCreateDTO;
@@ -7,6 +8,7 @@ import com.misis.archapp.user.dto.UserDTO;
 import com.misis.archapp.user.dto.UserUpdateDTO;
 import com.misis.archapp.user.dto.mapper.UserMapper;
 import com.misis.archapp.user.service.cache.UserCacheService;
+import com.misis.archapp.user.service.publisher.UserEventPublisher;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -25,16 +27,19 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final UserCacheService userCacheService;
+    private final UserEventPublisher userEventPublisher;
 
     @Autowired
     public UserService(
-        UserRepository userRepository,
-        UserMapper userMapper,
-        UserCacheService userCacheService
+            UserRepository userRepository,
+            UserMapper userMapper,
+            UserCacheService userCacheService,
+            UserEventPublisher userEventPublisher
     ) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.userCacheService = userCacheService;
+        this.userEventPublisher = userEventPublisher;
     }
 
     public List<UserDTO> getAllUsers() {
@@ -54,7 +59,7 @@ public class UserService {
         LOGGER.info("User cache miss");
         // cache miss - пользователя в кэше не оказалось
         UserDTO userFromDB = userRepository.findById(id).map(userMapper::toDTO)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         // актуализирует кэш значением из БД
         userCacheService.saveToCache(userFromDB);
@@ -64,12 +69,17 @@ public class UserService {
     public UserDTO createUser(UserCreateDTO userCreateDTO) {
         User user = userMapper.toEntity(userCreateDTO);
         User savedUser = userRepository.save(user);
+
+        // отправляет ивент с данными о пользователе
+        UserCreatedEvent userCreatedEvent = new UserCreatedEvent(user.getId(), user.getEmail(), user.getName());
+        userEventPublisher.publishUserEvent(userCreatedEvent);
+
         return userMapper.toDTO(savedUser);
     }
 
     public UserDTO updateUser(UUID id, UserUpdateDTO userUpdateDTO) {
         User user = userRepository.findById(id)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         if (userUpdateDTO.name().isPresent()) {
             user.setName(userUpdateDTO.name().get());
